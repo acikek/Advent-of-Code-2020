84e2047f2a7921406f5b60a58afefb7d88c8103d
acikek <spiralixp@gmail.com>
2020-12-03 12:14:51 -0800

    Oops

--- src/1/solution.js
+++ src/1/solution.js
@@ -12,8 +12,6 @@ for (let i = 0; i < numbers.length; i++) {
 
 // Part 2
 for (let i = 0; i < numbers.length; i++) {
-  let solution = false;
-
   for (let j = 0; j < numbers.length; j++) {
     for (let k = 0; k < numbers.length; k++) {
       if (+numbers[i] + +numbers[j] + +numbers[k] == 2020 && solutions.length < 2) {
